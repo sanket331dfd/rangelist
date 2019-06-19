6c9002e8e5b0db58c46e982838554f4cc612d32c
sanket331dfd <sanket331dfd@gmail.com>
2019-06-18 21:59:03 -0700

    updates for test setup

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /** A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
     A range list is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
