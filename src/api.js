fea531ecdcb0bb5e8b7142f1ddee1652479deb6a
sanket331dfd <51980214+sanket331dfd@users.noreply.github.com>
2019-06-18 20:09:51 -0700

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
+    A range list is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
+*/
+
+class RangeList {
+  /**
+    Adds a range to the list
+    @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+  */
+  add(range) {
+    // TODO: implement this
+  }
+
+  /**
+    Removes a range from the list
+    @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+  */
+  remove(range) {
+    // TODO: implement this
+  }
+
+  /**
+    Prints out the list of ranges in the range list
+  */
+  print() {
+    // TODO: implement this
+  }
+}
