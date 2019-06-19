51c1710c31742a173971f328b6c545fc62c36b1e
sanket331dfd <sanket331dfd@gmail.com>
2019-06-18 23:16:40 -0700

    next test fails

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@
 /** A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
     A range list is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
 */
+
+let toStr = function() {
+  return this.list;
+}
+
 class RangeList {
+  constructor() {
+    this.list = new Array()
+  }
+
   /**
     Adds a range to the list
     @param {Array<number>} range - Array of two integers that specify beginning and end of range.
@@ -24,8 +33,13 @@ class RangeList {
     Prints out the list of ranges in the range list
   */
   print() {
-    // TODO: implement this
+    console.log(toStr());
+  }
+
+  get() {
+    return this.list
   }
 }
+
 
 module.exports = RangeList;
--- test/test.js
+++ test/test.js
@@ -8,3 +8,11 @@ describe('Test instantiation', function () {
     expect(new RangeList()).exist;
     });
 });
+
+describe('Test valid add', function () {
+  it('should add a range', function () {
+     let r = new RangeList()
+     r.add([4,10])
+     expect(r.get().length).eq(1)
+     });
+ });
